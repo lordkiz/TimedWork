a2b65b4580a48c9b213e713755589b72ce56a009
Kizito Egeonu <kizito.egeonu@gmail.com>
2023-11-19 19:34:37 +0100

    position GetStarted Button

--- TimedWork/View Controllers/PreSetupViewController.swift
+++ TimedWork/View Controllers/PreSetupViewController.swift
@@ -13,6 +13,15 @@ class PreSetupViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpButton()
+    }
+    
+    func setUpButton() {
+        getStartedButton.translatesAutoresizingMaskIntoConstraints = false
+        getStartedButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        getStartedButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        getStartedButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        getStartedButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 300).isActive = true
     }
     
     
